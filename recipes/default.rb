bcf537e8dec29a09f3de6d30c069e0e304b9bbc1
David White <david@monkeyphp.com>
2014-05-06 20:34:14 +0100

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,24 +6,43 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "varnish-libs" do
+
+remote_file "#{Chef::Config[:file_cache_path]}/varnish-libs-4.0.0-1.el6.x86_64.rpm" do
     source "http://repo.varnish-cache.org/redhat/varnish-4.0/el6/x86_64/varnish/varnish-libs-4.0.0-1.el6.x86_64.rpm"
+    mode "0777"
+    action :create_if_missing
+    backup false
+end
+
+package "varnish-libs" do
+    source "#{Chef::Config[:file_cache_path]}/varnish-libs-4.0.0-1.el6.x86_64.rpm"
     provider Chef::Provider::Package::Rpm
     action :install
 end
 
-package "varnish" do
+package "jemalloc" do
+    action :install
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/varnish-4.0.0-1.el6.x86_64.rpm" do
     source "http://repo.varnish-cache.org/redhat/varnish-4.0/el6/x86_64/varnish/varnish-4.0.0-1.el6.x86_64.rpm"
+    mode "0777"
+    action :create_if_missing
+    backup false
+end
+
+package "varnish" do
+    source "#{Chef::Config[:file_cache_path]}/varnish-4.0.0-1.el6.x86_64.rpm"
     provider Chef::Provider::Package::Rpm
     action :install     
 end
 
 service "varnish" do
-    supports :start => true, :stop => true, :status => true, :restart => true :reload => true
+    supports :start => true, :stop => true, :status => true, :restart => true, :reload => true
     action :nothing
 end
 
-template "/etc/redis.conf" do 
+template "/etc/varnish/default.vcl" do 
     source   "default.vcl.erb"
     owner    "root"
     group    "root"
